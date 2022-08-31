818f2e8bc23f17973d7a6d4ab5553e6122b2c2f5
Roman <skinsrip21@liv.com>
2022-08-31 17:49:46 -0400

    First JUnit Test

--- social-media-spring/src/main/java/com/revature/services/UserService.java
+++ social-media-spring/src/main/java/com/revature/services/UserService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService implements UserServiceInterface {
 
     private final UserRepository userRepository;
 
