fa7ddf5588243066c8f302db12b774a03a4a29b7
ceaselessphantom <90485964+ceaselessphantom@users.noreply.github.com>
2022-09-01 10:58:07 -0400

    finished search

--- social-media-spring/src/main/java/com/revature/services/UserService.java
+++ social-media-spring/src/main/java/com/revature/services/UserService.java
@@ -4,6 +4,7 @@ import com.revature.models.User;
 import com.revature.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,10 +26,12 @@ public class UserService {
     }
     
     public List<User> searchUsers(String search){
-    	List<User> users = userRepository.findAll();
+    	List<User> users = new ArrayList<User>();
+    	users.addAll(userRepository.findAll());
+    	search=search.toLowerCase();
     	String username;
     	for(int x=0;x<users.size();x++) {
-    		username=users.get(x).getUsername();
+    		username=users.get(x).getUsername().toLowerCase();
     		int y, w;
     		charMatch:
     		for(y=0, w=0;y<search.length();y++) {
@@ -42,6 +45,7 @@ public class UserService {
     		}
     		if (w==username.length()) {
     			users.remove(users.get(x));
+    			x--;
     		}
     	}
     	return users;
