6f2aba66d8b2155b71a11f0fe772b223b0fe996e
unknown <chandlercarnes@gmail.com>
2022-09-07 00:31:44 -0400

    all mvp endpoint are functional, followers return error(not MVP)

--- social-media-spring/src/main/java/com/revature/repositories/UserRepository.java
+++ social-media-spring/src/main/java/com/revature/repositories/UserRepository.java
@@ -19,7 +19,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 	 */
     Optional<User> findByUsernameAndPassword(String username, String password);
     
-    Optional<User> findByUsernameAndFirstNameAndLastName(String username, String firstName, String lastName);
+    Optional<User> findByUsername(String username);
     
 	Optional<User> findByEmail(String email);
 }
