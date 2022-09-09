d7fa6c81576f305d394bd0c70ac936a9d02f7157
JFrazier <joshua.frazier1519@gmail.com>
2022-09-09 13:50:16 -0600

    added images

--- social-media-spring/src/main/resources/data.sql
+++ social-media-spring/src/main/resources/data.sql
@@ -1,26 +1,27 @@
-INSERT INTO users (email, password, first_name, last_name, username) VALUES (
+INSERT INTO users (email, password, first_name, last_name, username, profile_pic) VALUES (
     'testuser@gmail.com',
     'password',
     'Test',
     'User',
-    'Username'
+    'Username',
+    'https://i.imgur.com/ICK2lr1.jpeg'
 );
 INSERT INTO users(email, password, first_name, last_name, username, profile_pic)
 VALUES ('kylereese@gmail.com', 'password', 'kyle', 'reese', 'KRichy123', 'https://i.imgur.com/XUbwQ5E.jpeg');
-INSERT INTO users(email, password, first_name, last_name, username) 
-VALUES ('mattbuchanan@gmail.com', 'password', 'matt', 'buchanan', 'MBerrysan23');
-INSERT INTO users(email, password, first_name, last_name, username) 
-VALUES ('gingerventura@gmail.com', 'password', 'ginger', 'ventura', 'GVentelli234');
-INSERT INTO users(email, password, first_name, last_name, username) 
-VALUES ('calebbousman@gmail.com', 'password', 'caleb', 'bousman', 'CBoose634');
-INSERT INTO users(email, password, first_name, last_name, username) 
-VALUES ('alangrant@gmail.com', 'password', 'alan', 'grant', 'Alanant52');
-INSERT INTO users(email, password, first_name, last_name, username) 
-VALUES ('ianmalcolm@gmail.com', 'password', 'ian', 'malcolm', 'Ialcone34');
-INSERT INTO users(email, password, first_name, last_name, username) 
-VALUES ('johnhammond@gmail.com', 'password', 'john', 'hammond', 'user7');
-INSERT INTO users(email, password, first_name, last_name, username) 
-VALUES ('nlyman0120@gmail.com', 'password', 'Bob', 'Jack', 'user8');
+INSERT INTO users(email, password, first_name, last_name, username, profile_pic) 
+VALUES ('mattbuchanan@gmail.com', 'password', 'matt', 'buchanan', 'MBerrysan23', 'https://i.imgur.com/riE0O8R.png');
+INSERT INTO users(email, password, first_name, last_name, username, profile_pic) 
+VALUES ('gingerventura@gmail.com', 'password', 'ginger', 'ventura', 'GVentelli234', 'https://i.imgur.com/ZAb8lQF.jpeg');
+INSERT INTO users(email, password, first_name, last_name, username, profile_pic) 
+VALUES ('calebbousman@gmail.com', 'password', 'caleb', 'bousman', 'CBoose634', 'https://i.imgur.com/yQeXp8Q.jpeg');
+INSERT INTO users(email, password, first_name, last_name, username, profile_pic) 
+VALUES ('alangrant@gmail.com', 'password', 'alan', 'grant', 'Alanant52', 'https://i.imgur.com/5eMAuXg.jpeg');
+INSERT INTO users(email, password, first_name, last_name, username, profile_pic) 
+VALUES ('ianmalcolm@gmail.com', 'password', 'ian', 'malcolm', 'Ialcone34', 'https://i.imgur.com/qMVVzkT.jpeg');
+INSERT INTO users(email, password, first_name, last_name, username, profile_pic) 
+VALUES ('johnhammond@gmail.com', 'password', 'john', 'hammond', 'user7', 'https://i.imgur.com/xYrSm2G.jpeg');
+INSERT INTO users(email, password, first_name, last_name, username, profile_pic) 
+VALUES ('nlyman0120@gmail.com', 'password', 'Bob', 'Jack', 'user8', 'https://i.imgur.com/MlCQEhQ.png');
 
 INSERT INTO emoji (emoji_name, emoji_pic) VALUES ('thumbsup', 'https://images.emojiterra.com/google/noto-emoji/v2.034/512px/1f44d.png');
 INSERT INTO emoji (emoji_name, emoji_pic) VALUES ('laughing', 'https://toppng.com/uploads/preview/emoji-transparent-laughing-emoji-11550234631y6jwxckdn1.png');
