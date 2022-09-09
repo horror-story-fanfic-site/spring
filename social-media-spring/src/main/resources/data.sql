248f224d2f0ba22cc1876a88f3bdac13488b2d30
JFrazier <joshua.frazier1519@gmail.com>
2022-09-09 10:49:50 -0600

    added pics to emojis

--- social-media-spring/src/main/resources/data.sql
+++ social-media-spring/src/main/resources/data.sql
@@ -5,27 +5,27 @@ INSERT INTO users (email, password, first_name, last_name, username) VALUES (
     'User',
     'Username'
 );
-INSERT INTO users(email, password, first_name, last_name, username)
-VALUES ('kylereese@gmail.com', 'password', 'kyle', 'reese', 'user1');
+INSERT INTO users(email, password, first_name, last_name, username, profile_pic)
+VALUES ('kylereese@gmail.com', 'password', 'kyle', 'reese', 'KRichy123', 'https://i.imgur.com/XUbwQ5E.jpeg');
 INSERT INTO users(email, password, first_name, last_name, username) 
-VALUES ('mattbuchanan@gmail.com', 'password', 'matt', 'buchanan', 'user2');
+VALUES ('mattbuchanan@gmail.com', 'password', 'matt', 'buchanan', 'MBerrysan23');
 INSERT INTO users(email, password, first_name, last_name, username) 
-VALUES ('gingerventura@gmail.com', 'password', 'ginger', 'ventura', 'user3');
+VALUES ('gingerventura@gmail.com', 'password', 'ginger', 'ventura', 'GVentelli234');
 INSERT INTO users(email, password, first_name, last_name, username) 
-VALUES ('calebbousman@gmail.com', 'password', 'caleb', 'bousman', 'user4');
+VALUES ('calebbousman@gmail.com', 'password', 'caleb', 'bousman', 'CBoose634');
 INSERT INTO users(email, password, first_name, last_name, username) 
-VALUES ('alangrant@gmail.com', 'password', 'alan', 'grant', 'user5');
+VALUES ('alangrant@gmail.com', 'password', 'alan', 'grant', 'Alanant52');
 INSERT INTO users(email, password, first_name, last_name, username) 
-VALUES ('ianmalcolm@gmail.com', 'password', 'ian', 'malcolm', 'user6');
+VALUES ('ianmalcolm@gmail.com', 'password', 'ian', 'malcolm', 'Ialcone34');
 INSERT INTO users(email, password, first_name, last_name, username) 
 VALUES ('johnhammond@gmail.com', 'password', 'john', 'hammond', 'user7');
 INSERT INTO users(email, password, first_name, last_name, username) 
 VALUES ('nlyman0120@gmail.com', 'password', 'Bob', 'Jack', 'user8');
 
-INSERT INTO emoji (emoji_name, descript) VALUES ('thumbsup', 'its a like');
-INSERT INTO emoji (emoji_name, descript) VALUES ('laughing', 'its a a laughing face');
-INSERT INTO emoji (emoji_name, descript) VALUES ('scared', 'its a scared face');
-INSERT INTO emoji (emoji_name, descript) VALUES ('wow', 'its a wow face');
+INSERT INTO emoji (emoji_name, emoji_pic) VALUES ('thumbsup', 'https://images.emojiterra.com/google/noto-emoji/v2.034/512px/1f44d.png');
+INSERT INTO emoji (emoji_name, emoji_pic) VALUES ('laughing', 'https://toppng.com/uploads/preview/emoji-transparent-laughing-emoji-11550234631y6jwxckdn1.png');
+INSERT INTO emoji (emoji_name, emoji_pic) VALUES ('scared', 'https://toppng.com/uploads/preview/omg-face-emoji-png-sh-11545508210l9uqnywdn1.png');
+INSERT INTO emoji (emoji_name, emoji_pic) VALUES ('wow', 'https://e7.pngegg.com/pngimages/496/17/png-clipart-computer-icons-emoticon-smiley-like-button-smiley-miscellaneous-smiley.png');
 
 INSERT INTO posts (id, text, image_url, author_id) VALUES (
     10000,
