043dc297fc6253fbce052637a9eef2b9d59f7ce8
Caleb Bousman <calebbousman@gmail.com>
2022-09-10 19:25:59 -0400

    update

--- social-media-spring/src/main/java/com/revature/models/Post.java
+++ social-media-spring/src/main/java/com/revature/models/Post.java
@@ -24,19 +24,10 @@ import lombok.NoArgsConstructor;
 public class Post {
 
 	@Id
-<<<<<<< HEAD
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-=======
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-	
-	@Column(name="text", nullable=true, length=70000)
-	private String text;
-	
->>>>>>> 46c8698274d2de5ae2531cbcece467f2fc416484
 
-	@Column(name = "text", nullable = true)
+	@Column(name = "text", nullable = true, length = 70000)
 	private String text;
 
 	@Column(name = "image_url", nullable = true)
