4b2c393ef96035ccd35fea0c43185e6c1f83e57d
ceaselessphantom <90485964+ceaselessphantom@users.noreply.github.com>
2022-09-12 15:11:20 -0400

    added comments

--- social-media-spring/src/main/java/com/revature/models/front/FrontEmoji.java
+++ social-media-spring/src/main/java/com/revature/models/front/FrontEmoji.java
@@ -6,6 +6,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/***
+ * 
+ * @author phantom
+ * This is for returning the values to user in an efficient manner.
+ * Count is how many times the emoji is in use on the post.
+ * you is if the current user is among the count.
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
